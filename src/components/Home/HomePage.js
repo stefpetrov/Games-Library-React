18335513f039b918f90a0a40ac9822dafa7e6574
Stefan Petrov <stefspetrov@gmail.com>
2022-09-19 15:30:05 +0300

    relative paths changes

--- src/components/Home/HomePage.js
+++ src/components/Home/HomePage.js
@@ -20,7 +20,8 @@ const HomePage = () => {
                 <h2>ALL new games are</h2>
                 <h3>Only in GamesLib</h3>
             </div>
-            <img src="/images/four_slider_img01.PNG" alt="hero" />
+            <img src="/images/four_slider_img01.png" alt="hero" />
+
 
             <div id="home-page">
                 <h1>Latest Games</h1>
